c115b3d1e67ec609b3ffd03ef602b1256e74577b
Money God ONE [MGO] <moneygodone@outlook.com>
2026-01-02 08:05:55 +0000

    Change header rotating O

--- src/app/landing-page/components/Header.tsx
+++ src/app/landing-page/components/Header.tsx
@@ -4,7 +4,7 @@ import RotatingO from './RotatingO';
 
 export default function Header() {
   return (
-    <header className="relative z-10 flex flex-col items-center justify-center min-h-[45vh] text-center">
+    <header className="relative z-10 flex flex-col items-center justify-center min-h-[45vh] text-center -mt-12">
   <h1 className="text-6xl md:text-7xl font-extrabold tracking-tight text-white">
     Elysium
     <RotatingO />

db3120b1e5a0f0f98c880e57d0371a79cc76a44f
Money God ONE [MGO] <moneygodone@outlook.com>
2026-01-02 08:01:44 +0000

    Change header rotating O

--- src/app/landing-page/components/Header.tsx
+++ src/app/landing-page/components/Header.tsx
@@ -4,9 +4,10 @@ import RotatingO from './RotatingO';
 
 export default function Header() {
   return (
-    <header className="relative z-[9999] bg-red-500">
-  <h1 className="text-6xl text-white">
-    HEADER TEST
+    <header className="relative z-10 flex flex-col items-center justify-center min-h-[45vh] text-center">
+  <h1 className="text-6xl md:text-7xl font-extrabold tracking-tight text-white">
+    Elysium
+    <RotatingO />
   </h1>
 </header>
   );

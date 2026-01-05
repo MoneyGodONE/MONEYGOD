72670dbfc5c80cc9679cbedb294b0d5502f1f732
Money God ONE [MGO] <moneygodone@outlook.com>
2026-01-05 16:47:29 +0700

    Update WebGLBackground.tsx

--- src/app/landing-page/components/WebGLBackground.tsx
+++ src/app/landing-page/components/WebGLBackground.tsx
@@ -7,9 +7,6 @@ export default function WebGLBackground() {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (window.innerWidth < 768) {
-  canvas.style.display = 'none';
-  return;
 }
     if (!canvas) return;
 

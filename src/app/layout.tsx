9d104ad0a292f355212a6a0a6be4376fa22ac4e3
Money God ONE [MGO] <moneygodone@outlook.com>
2026-02-09 20:24:08 +0700

    Integrate Google Analytics into layout
    Added Google Analytics scripts for tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/index.css';
+import Script from 'next/script';
 
 export const viewport = {
   width: 'device-width',
@@ -25,6 +26,23 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-transparent overflow-x-hidden">
         {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-58NS4ZFQH7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-58NS4ZFQH7', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+        
       </body>
     </html>
   );
